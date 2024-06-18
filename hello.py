86988702468bae545ae8cc08cea0c4949cf1dae9
NakyZ <ariyo.naky08@gmail.com>
2024-06-18 11:41:04 +0700

    belajar python

--- hello.py
+++ hello.py
@@ -1,2 +1,16 @@
-x = "nakyZ"
-print(x)
+print("Hallo Yeheskiel")
+
+def greeting():
+    return "Welcome To Python"
+
+print(greeting())
+
+import datetime
+now = datetime.datetime.now()
+print(now)
+
+class Greeting: 
+    def greet(self,name):
+        return "Hello" + name   
+
+print(greeting().greet("Naky"))
---
+++ readme.md
@@ -0,0 +1,1 @@
+## Disini, kami sedang mempelajari system dari Bahasa Python. "18 Juni 2024"
